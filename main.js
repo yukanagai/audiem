9b706263e90f1a33577655398dc6fba4be6c23fc
Yuka Nagai <yukanagai@gmail.com>
2015-11-05 17:29:54 -0500

    got instagram api working

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var MainApp = React.createClass({
 		return (
 			<div>
 				<Title />
-				<SearchBar onInstagramSearch={this.handleInstagramSearch}  />
+				<MainBody />
 			</div>
 		);
 	}
@@ -23,12 +23,14 @@ var Title = React.createClass ({
 var SearchBar = React.createClass({
 	handleSubmit: function(event) {
 		event.preventDefault();
+		var hashtag = this.refs.hashtag.value;
+		console.log(hashtag)
 		this.props.onInstagramSearch(hashtag);
 	},
 	render: function() {
 		return ( 
 			<form onSubmit={this.handleSubmit}>
-				<input type="text" name="artistname" placeholder="Enter, song, artists, genre..." />
+				<input type="text" name="artistname" ref="hashtag" placeholder="Enter, song, artists, genre..." />
 			</form>
 		);
 	}
@@ -36,21 +38,58 @@ var SearchBar = React.createClass({
 
 var MainBody = React.createClass({
 	getInitialState: function() {
-		return {hashtag: null}
+		return {hashtag: null, imageURs: null}
+	},
+	handleInstagramSearch: function(hashtag) {
+		this.InstagramSearch(hashtag);
 	},
 	InstagramSearch: function(hashtag) {
-		console.log("THIS IS WORKING");
+		this.setState({hashtag: hashtag});
+		console.log("THIS IS WORKING", hashtag);
+		$.ajax({
+			url: "https://api.instagram.com/v1/tags/"+hashtag+"/media/recent?client_id=91bd759fe1d84066ace0dae19428c7c6",
+			dataType: 'jsonp',
+			success: function(result) {
+
+				
+				console.log(result.data);
+			}
+		});
 	},
 	render: function() {
 		return (
 			<div>
-				<SpotifyPlayer />
-				<InstagramBackground />
-
+				<SearchBar onInstagramSearch={this.handleInstagramSearch}  />
+				<SpotifyPlayer hashtag={this.state.hashtag} />
+				<InstagramBackground hashtag={this.state.hashtag} />
 			</div>
 		);
 	}
 });
+
+var InstagramBackground = React.createClass({
+	
+	render: function() {
+		return (
+			<div>
+			</div>
+		);
+	}
+});
+
+
+var SpotifyPlayer = React.createClass({
+	render: function() {
+		return (
+			<div>
+				SPOTIFY PLAYER: {this.props.hashtag}
+			</div>
+		);
+	}
+});
+
+
+
 
 
 ReactDOM.render(<MainApp />, document.getElementById('content') );
