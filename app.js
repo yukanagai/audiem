1ecc5f1e0dd306ff8522963bbb884cd21b604c00
Meiji Chen <meiji.chen@gmail.com>
2015-11-06 09:54:03 -0500

    commit after pull from master

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var Instagram = React.createClass({
-  
+  getInitialState: function(){
+    return {data: []}
+  },
+
+  componentDidMount: function(){
+    this.loadImages(this.props.instaHashtag)
+  },
+
+  loadImages: function(search){
+    $.ajax({
+      url: 'https://api.instagram.com/v1/tags/'+ search + /media/recent?client_id=91bd759fe1d84066ace0dae19428c7c6'
+    })
+  }
 })
